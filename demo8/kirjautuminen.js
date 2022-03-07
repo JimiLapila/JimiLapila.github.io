c83b3604cc519a6dec9e4ccac6f575f35b06ec8f
JimiLapila <jimi.lapila@edu.keuda.fi>
2022-03-07 11:06:02 +0200

    demo8

--- demo8/kirjautuminen.js
+++ demo8/kirjautuminen.js
@@ -1,13 +1,23 @@
 document.addEventListener("DOMContentLoaded", onkoKirjautunut);
 
 function onkoKirjautunut(){
-    if(localStorage.getItem("kirjautunut") === "kylla") {
+    if(localStorage.getItem("kirjautunut") === 'kylla') {
     document.getElementById('tervetuloa_teksti').textContent += localStorage.getItem("nimi");
     document.getElementById("kirjaudu_nappi").style.display = "none";
+    } else {
+        document.getElementById('tervetuloa_teksti').textContent;
+        document.getElementById("kirjautumis_nappi");
+        document.getElementById("ulos_kirjautuminen").style.display = "none";
     }
 }
+
 
 function kirjaudu(){
     localStorage.setItem("nimi", document.getElementById("nimi").value);
     localStorage.setItem("kirjautunut", "kylla");
+}
+
+function ulosKirjautuminen(){
+    localStorage.removeItem("nimi", document.getElementById("nimi").value);
+    localStorage.removeItem("kirjautunut", "ei");
 }
